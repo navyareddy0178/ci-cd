dee9e9f822580fadf40bb63dd95d06f6702bd94a
navyareddy0178 <annaji.navyareddy@gmail.com>
2025-08-07 09:39:37 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,6 +1,7 @@
 class Helloworld{
     public static void main(String[] args){
-        System.out.println("I am Navya && i am a good girl & i am thr cool person")
+        System.out.println("I am Navya && i am a good girl & i am thr cool person");
     }
 
 }
+
